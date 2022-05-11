ac2423021116297f43aa06dab5df90ce07cb7697
Raman Sharma <raman.sharma@global.ntt>
2022-05-11 10:09:09 +1000

    fix configuration setting

--- licensing-service/src/test/java/com/optimagrowth/license/LicensingServiceApplicationTests.java
+++ licensing-service/src/test/java/com/optimagrowth/license/LicensingServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.optimagrowth.license;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+  properties = {"spring.cloud.config.enabled=false"}
+)
 class LicensingServiceApplicationTests {
 
 	@Test
